c3fce8859fda080a7d12c674965f13ce964a51d2
ranasobeid95 <ranasobeid95@gmail.com>
2021-03-08 21:14:16 +0200

    Make request to get details for country by name from params #4

--- src/app/components/countries/country-details/country-details.component.ts
+++ src/app/components/countries/country-details/country-details.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { CountriesService } from '../countries.service';
+import { Country } from '../country';
 
 @Component({
   selector: 'country-details',
   templateUrl: './country-details.component.html',
-  styleUrls: ['./country-details.component.css']
+  styleUrls: ['./country-details.component.css'],
 })
 export class CountryDetailsComponent implements OnInit {
-
-  constructor() { }
+  country: Country[] = [];
+  constructor(
+    private countriesService: CountriesService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    this.route.params.subscribe(({ name }) => {
+      this.getDetails(name);
+    });
   }
 
+  getDetails(name: string) {
+    this.countriesService
+      .getCountryByName(name)
+      .subscribe((response: Country[]) => {
+        this.country = response;
+      });
+  }
 }

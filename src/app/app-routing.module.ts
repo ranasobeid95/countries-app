bb51c5f8ea1d5c4219080513ff9595aefb9da0e3
ranasobeid95 <ranasobeid95@gmail.com>
2021-03-08 20:35:38 +0200

    add path for home page use of lazy loading for Contries Module add path for 404 page #4

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './components/shared/page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'countries',
+    pathMatch: 'full',
+  },
+  {
+    path: 'countries',
+    loadChildren: () =>
+      import('./components/countries/countries.module').then(
+        (m) => m.CountriesModule
+      ),
+  },
+  { path: '**', component: PageNotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}

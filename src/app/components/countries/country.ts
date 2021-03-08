9787bca44487e965978f96f7aeb7bd2fea2f97e2
ranasobeid95 <ranasobeid95@gmail.com>
2021-03-08 20:47:57 +0200

    create interface for country data #4

--- src/app/components/countries/country.ts
+++ src/app/components/countries/country.ts
@@ -1,7 +1,13 @@
 export interface Country {
-    name:string;
-    capital:string;
-    region:string;
-    flag:string;
-    borders:string[];
+  name: string;
+  capital: string;
+  region: string;
+  subregion: string;
+  flag: string;
+  borders: string[];
+  population: number;
+  topLevelDomain: string;
+  nativeName: string;
+  currencies: string;
+  languages: string[];
 }

02cab6dc42d465c69478eb7374965d89dbe29ee1
ranasobeid95 <ranasobeid95@gmail.com>
2021-03-08 20:39:41 +0200

    add path for all countries page and details page #4

--- src/app/components/countries/countries-routing.module.ts
+++ src/app/components/countries/countries-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CountryDetailsComponent } from './country-details/country-details.component';
+import { CountriesComponent } from './countries.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: CountriesComponent },
+  { path: ':name', component: CountryDetailsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class CountriesRoutingModule { }
+export class CountriesRoutingModule {}

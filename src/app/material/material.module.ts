7d26f6adc0fe2358de754553357b72769f971f38
ranasobeid95 <ranasobeid95@gmail.com>
2021-03-12 14:16:42 +0200

    import and export material components #6

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,8 +1,29 @@
 import { NgModule } from '@angular/core';
+
+import { MatSliderModule } from '@angular/material/slider';
+import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatSelectModule } from '@angular/material/select';
 
-const MaterialComponents = [MatToolbarModule];
-
+const MaterialComponents = [
+  MatSliderModule,
+  MatButtonModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatCardModule,
+  MatAutocompleteModule,
+  FormsModule,
+  ReactiveFormsModule,
+  MatProgressSpinnerModule,
+  MatSnackBarModule,
+  MatSelectModule,
+];
 @NgModule({
   imports: [MaterialComponents],
   exports: [MaterialComponents],

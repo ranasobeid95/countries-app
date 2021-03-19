a81c7863f8ac1beffd7e42b3ce89b52bd13b6dd7
ranasobeid95 <ranasobeid95@gmail.com>
2021-03-19 08:33:50 +0200

    add HttpClientTestingModule to test file #25

--- src/app/components/countries/countries.component.spec.ts
+++ src/app/components/countries/countries.component.spec.ts
@@ -1,16 +1,40 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 
 import { CountriesComponent } from './countries.component';
+import { CountriesService } from './services/countries.service';
+
+describe('CountriesService', () => {
+  let httpTestingController: HttpTestingController;
+  let service: CountriesService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [CountriesComponent],
+      providers: [CountriesService],
+      imports: [HttpClientTestingModule],
+    });
+  });
+
+  httpTestingController = TestBed.get(HttpTestingController);
+  service = TestBed.get(CountriesService);
+  afterEach(() => {
+    httpTestingController.verify();
+  });
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
 
 describe('CountriesComponent', () => {
   let component: CountriesComponent;
   let fixture: ComponentFixture<CountriesComponent>;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ CountriesComponent ]
-    })
-    .compileComponents();
+    await TestBed.compileComponents();
   });
 
   beforeEach(() => {

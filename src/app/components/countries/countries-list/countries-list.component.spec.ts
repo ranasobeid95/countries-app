03bbeabaea8a828434bc00af8157e30349a6521c
ranasobeid95 <ranasobeid95@gmail.com>
2021-03-22 04:22:39 +0200

    Make test for no-data div will show when country is empty #25

--- src/app/components/countries/countries-list/countries-list.component.spec.ts
+++ src/app/components/countries/countries-list/countries-list.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CountriesListComponent } from './countries-list.component';
+import { DebugElement } from '@angular/core';
 
 describe('CountriesListComponent', () => {
   let component: CountriesListComponent;
@@ -20,5 +21,13 @@ describe('CountriesListComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('h2 element should contain the country name  ', () => {
+    component.listCountries = [];
+    const listDe: DebugElement = fixture.debugElement;
+    const listEle: HTMLElement = listDe.nativeElement;
+    const h3 = listEle.querySelector('.no-data h3');
+    expect(h3?.textContent).toBe('No Countries Found');
   });
 });

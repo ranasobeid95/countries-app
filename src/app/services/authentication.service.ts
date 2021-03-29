d360bd6e897182c51a69987b81cb6ceafb7a4fc5
ranasobeid95 <ranasobeid95@gmail.com>
2021-03-30 00:52:39 +0300

    Injected AngularFireAuth class into the constructor Add createAccount function and use createUserWithEmailAndPassword to create new account Add login function and use signInWithEmailAndPassword to login #30

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthenticationService {
+  constructor(public afAuth: AngularFireAuth) {}
 
-  constructor() { }
+  async createAccount(email: string, password: string) {
+    try {
+      const result = await this.afAuth.createUserWithEmailAndPassword(
+        email,
+        password
+      );
+      console.log('Create Account', result);
+      return !!result;
+    } catch (error) {
+      return false;
+    }
+  }
+
+  async login(email: string, password: string) {
+    try {
+      const result = await this.afAuth.signInWithEmailAndPassword(
+        email,
+        password
+      );
+      return !!result;
+    } catch (error) {
+      return false;
+    }
+  }
 }

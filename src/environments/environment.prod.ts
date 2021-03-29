6d84d0612ba9ca7cd814c100747991285370e335
ranasobeid95 <ranasobeid95@gmail.com>
2021-03-30 00:44:02 +0300

    add firesbase to to enviroment varible #30

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
   production: true,
+  firebase: {
+    apiKey: 'AIzaSyBwW77hknVDe5nHfX-Z8DfPxvpwd47zjiI',
+    databaseURL: 'https://where-in-the-world-dee98.firebaseio.com',
+    authDomain: 'where-in-the-world-dee98.firebaseapp.com',
+    projectId: 'where-in-the-world-dee98',
+    storageBucket: 'where-in-the-world-dee98.appspot.com',
+    messagingSenderId: '942092730013',
+    appId: '1:942092730013:web:da98b4cb054eba27ac5e74',
+    measurementId: 'G-VR67X6Z5H9',
+  },
 };

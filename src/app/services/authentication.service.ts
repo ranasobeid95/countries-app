9f599ef9dd97d595c899d21db3c59f08def23ff0
ranasobeid95 <ranasobeid95@gmail.com>
2021-03-30 01:39:48 +0300

    Inject Firestore service Add saveInLocalStorge method to Saving user data in localstorage when logged in and setting up null when logged out #30

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -1,11 +1,31 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { User } from '../model/user';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthenticationService {
-  constructor(public afAuth: AngularFireAuth) {}
+  userData: any;
+  constructor(
+    public afAuth: AngularFireAuth,
+    public afStore: AngularFirestore
+  ) {
+    this.afAuth.authState.subscribe((user) => {
+      this.saveInLocalStorage(user);
+    });
+  }
+
+  saveInLocalStorage(user: any) {
+    if (user) {
+      this.userData = user;
+      localStorage.setItem('user', JSON.stringify(this.userData));
+    } else {
+      localStorage.setItem('user', null!);
+      JSON.stringify(localStorage.getItem('user'));
+    }
+  }
 
   async createAccount(email: string, password: string) {
     try {

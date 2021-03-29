e0292b46404491b23b3994012c37a9825b8f81c8
ranasobeid95 <ranasobeid95@gmail.com>
2021-03-29 16:31:21 +0300

    Setup NgModule for the AngularFireModule Add modules for the individual NgModules that your application needs #30

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
 
+import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 import { SharedModule } from './components/shared/shared.module';
 import { CountriesModule } from './components/countries/countries.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
 import { HttpErrorInterceptor } from './http-interceptors/http-interceptors.interceptor';
 
@@ -21,6 +24,9 @@ import { HttpErrorInterceptor } from './http-interceptors/http-interceptors.inte
     MaterialModule,
     SharedModule,
     CountriesModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
   ],
   providers: [
     {

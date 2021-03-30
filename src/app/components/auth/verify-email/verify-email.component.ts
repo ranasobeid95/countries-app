053c324377c2b6b9424c5a3c7891a4693e275c8e
ranasobeid95 <ranasobeid95@gmail.com>
2021-03-30 21:55:32 +0300

    Create the AuthComponent #31

---
+++ src/app/components/auth/verify-email/verify-email.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'verify-email',
+  templateUrl: './verify-email.component.html',
+  styleUrls: ['./verify-email.component.css']
+})
+export class VerifyEmailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

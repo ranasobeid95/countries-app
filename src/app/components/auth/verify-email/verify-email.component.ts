46d3569b0c61be0190bca98c38de68870ebe8c53
ranasobeid95 <ranasobeid95@gmail.com>
2021-04-01 03:27:01 +0300

    add tamplet and style for elements #31

--- src/app/components/auth/verify-email/verify-email.component.ts
+++ src/app/components/auth/verify-email/verify-email.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from 'src/app/services/authentication.service';
 
 @Component({
   selector: 'verify-email',
   templateUrl: './verify-email.component.html',
-  styleUrls: ['./verify-email.component.css']
+  styleUrls: ['./verify-email.component.css'],
 })
 export class VerifyEmailComponent implements OnInit {
+  constructor(public ngAuthService: AuthenticationService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }

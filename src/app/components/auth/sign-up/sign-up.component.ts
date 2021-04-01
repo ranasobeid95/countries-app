949a1ebbcc8083a6b36430e76d34612bb3a66eca
ranasobeid95 <ranasobeid95@gmail.com>
2021-04-01 03:40:25 +0300

    use FormGroub and FormControl in form validation injected authService on constructor use signUp method and pass the form value as User object reset form after signIn successfully #31

--- src/app/components/auth/sign-up/sign-up.component.ts
+++ src/app/components/auth/sign-up/sign-up.component.ts
@@ -1,15 +1,74 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  Validators,
+  FormGroup,
+  FormBuilder,
+  FormControl,
+} from '@angular/forms';
+import { mustMatch } from '../../../helper/must-match.validator';
+import { AuthenticationService } from 'src/app/services/authentication.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'sign-up',
   templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.css']
+  styleUrls: ['./sign-up.component.css'],
 })
 export class SignUpComponent implements OnInit {
+  signUpForm!: FormGroup;
+  submitted = false;
+  hide = true;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthenticationService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
+    this.signUpForm = this.formBuilder.group(
+      {
+        fullName: new FormControl('', {
+          validators: [Validators.required],
+          updateOn: 'blur',
+        }),
+        email: new FormControl('', {
+          validators: [Validators.required, Validators.email],
+          updateOn: 'blur',
+        }),
+        password: new FormControl('', {
+          validators: [Validators.required, Validators.minLength(8)],
+          updateOn: 'blur',
+        }),
+        confirmPassword: new FormControl('', {
+          validators: [Validators.required],
+          updateOn: 'blur',
+        }),
+      },
+
+      {
+        validator: mustMatch('password', 'confirmPassword'),
+      }
+    );
   }
 
+  get f() {
+    return this.signUpForm.controls;
+  }
+
+  submit() {
+    this.submitted = true;
+    if (!this.signUpForm.valid) {
+      return;
+    }
+    this.authService.signUp(this.signUpForm.value).then(() => {
+      this.onReset();
+      this.router.navigate(['/verify-email']);
+    });
+  }
+
+  onReset() {
+    this.submitted = false;
+    this.signUpForm.reset();
+  }
 }

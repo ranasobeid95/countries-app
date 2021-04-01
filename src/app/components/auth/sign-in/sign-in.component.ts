6209da02a5d04bf3feebd1eee3e793a05e729e4e
ranasobeid95 <ranasobeid95@gmail.com>
2021-04-01 03:37:17 +0300

    use FormGroub and FormControl in form validation create a method for getting and use controls easily injected authService on constructor reset form after signIn successfully #31

--- src/app/components/auth/sign-in/sign-in.component.ts
+++ src/app/components/auth/sign-in/sign-in.component.ts
@@ -1,15 +1,68 @@
 import { Component, OnInit } from '@angular/core';
-
+import {
+  Validators,
+  FormGroup,
+  FormBuilder,
+  FormControl,
+} from '@angular/forms';
+import { AuthenticationService } from 'src/app/services/authentication.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'sign-in',
   templateUrl: './sign-in.component.html',
-  styleUrls: ['./sign-in.component.css']
+  styleUrls: ['./sign-in.component.css'],
 })
 export class SignInComponent implements OnInit {
+  signInForm!: FormGroup;
+  submitted = false;
+  isNotExist = false;
+  invalidPassword = false;
+  hide = true;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthenticationService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
+    this.signInForm = this.formBuilder.group({
+      email: new FormControl('', {
+        validators: [Validators.required, Validators.email],
+        updateOn: 'blur',
+      }),
+      password: new FormControl('', {
+        validators: [Validators.required],
+        updateOn: 'blur',
+      }),
+    });
   }
 
+  get f() {
+    return this.signInForm.controls;
+  }
+
+  submit() {
+    this.submitted = true;
+    if (!this.signInForm.valid) {
+      return;
+    }
+    this.authService
+      .signIn(this.signInForm.value)
+      .then((res) => {
+        this.onReset();
+      })
+      .catch((err) => {
+        if (err.message.includes('no user record')) {
+          this.isNotExist = true;
+        } else {
+          this.invalidPassword = true;
+        }
+      });
+  }
+
+  onReset() {
+    this.submitted = false;
+    this.signInForm.reset();
+  }
 }

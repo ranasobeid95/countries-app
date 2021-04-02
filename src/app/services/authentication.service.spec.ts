f49f2834b96a81384281d6c10441de94849617ee
ranasobeid95 <ranasobeid95@gmail.com>
2021-04-02 11:07:44 +0300

    Create mock sign-in/up/out function to to simulate real function without  register user info on firebase add test for creates new account add test for user is login with incorrect password add test for user logged out #31

--- src/app/services/authentication.service.spec.ts
+++ src/app/services/authentication.service.spec.ts
@@ -8,10 +8,99 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { MaterialModule } from '../material/material.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgZone } from '@angular/core';
+import { User } from '../model/user';
+import { users } from '../model/dummyData';
+import { async } from 'rxjs';
 
 describe('AuthenticationService', () => {
   let service: AuthenticationService;
+
+  let signInWithEmailAndPasswordSpy,
+    signOutSpy,
+    createUserWithEmailAndPasswordSpy: jasmine.Spy;
+
+  let userMock: {
+    uid: string;
+    email: string;
+    displayName?: string;
+    emailVerified?: boolean;
+  };
+
+  const allUser: User[] = users;
+
+  const angularFireAuthSpy = jasmine.createSpyObj(' AngularFireAuth', [
+    'signInWithEmailAndPassword',
+    'signOut',
+    'createUserWithEmailAndPassword',
+  ]);
+
+  function mockSignUpFunctions(values: User) {
+    createUserWithEmailAndPasswordSpy = angularFireAuthSpy.createUserWithEmailAndPassword.and.returnValue(
+      new Promise((resolve, reject) => {
+        let { email, fullName } = values;
+        let user = allUser.find((ele) => ele.email === email);
+        if (!user) {
+          userMock = {
+            uid: '3',
+            email,
+            displayName: fullName,
+            emailVerified: false,
+          };
+          const credentialsMock = {
+            user: userMock,
+          };
+          resolve(credentialsMock);
+        } else {
+          reject(
+            new Error('The email address is already in use by another account.')
+          );
+        }
+      })
+    );
+  }
+
+  function mockSignInFunctions(values: User) {
+    signInWithEmailAndPasswordSpy = angularFireAuthSpy.signInWithEmailAndPassword.and.returnValue(
+      new Promise((resolve, reject) => {
+        let { email, password } = values;
+        let user = allUser.find((ele) => ele.email === email);
+        if (user) {
+          if (user.password === password) {
+            userMock = {
+              uid: '1',
+              email: user.email,
+              displayName: user.fullName,
+              emailVerified: false,
+            };
+            const credentialsMock = {
+              user: userMock,
+            };
+            resolve(credentialsMock);
+          } else {
+            reject(new Error('The password is invalid'));
+          }
+        } else {
+          reject(
+            new Error('The email address is already in use by another account.')
+          );
+        }
+      })
+    );
+  }
+
+  function mockSignOut(email: string) {
+    signOutSpy = angularFireAuthSpy.signOut.and.returnValue(
+      new Promise((resolve, reject) => {
+        let user = allUser.find((ele) => ele.email === email);
+
+        if (user) {
+          resolve('out');
+        } else {
+          reject(new Error('Logged out failed'));
+        }
+      })
+    );
+  }
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -21,15 +110,95 @@ describe('AuthenticationService', () => {
         BrowserAnimationsModule,
         MaterialModule,
       ],
-      providers: [AngularFireAuth, AngularFirestore],
+      providers: [
+        { provide: AngularFireAuth, useValue: angularFireAuthSpy },
+        AngularFirestore,
+        RouterTestingModule,
+      ],
     });
     service = TestBed.inject(AuthenticationService);
   });
 
+  beforeEach(() => {});
+
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
-  it('Creates new account', async () => {});
 
-  it('User is logged in ', async () => {});
+  it('Creates new account', async () => {
+    let newUser: User = {
+      fullName: 'userTest',
+      email: 'test@test.com',
+      password: 'test123',
+    };
+    try {
+      mockSignUpFunctions(newUser);
+      const newUserCredential = await service.signUp(newUser);
+      const actualUser = {
+        uid: newUserCredential.user?.uid,
+        email: newUserCredential.user?.email,
+        displayName: newUserCredential.user?.displayName,
+        emailVerified: newUserCredential.user?.emailVerified,
+      };
+
+      expect(actualUser).toEqual({
+        uid: '3',
+        email: newUser.email,
+        displayName: newUser.fullName,
+        emailVerified: false,
+      });
+    } catch (error) {
+      expect(error).toBeFalsy();
+    }
+  });
+
+  it('User is logged in ', async () => {
+    let newUser: User = {
+      email: allUser[0].email,
+      password: allUser[0].password,
+    };
+
+    try {
+      mockSignInFunctions(newUser);
+
+      const newUserCredential = await service.signIn(newUser);
+      const actualUser = {
+        uid: newUserCredential.user?.uid,
+        email: newUserCredential.user?.email,
+        displayName: newUserCredential.user?.displayName,
+        emailVerified: newUserCredential.user?.emailVerified,
+      };
+
+      const credential = service.getUserCredential();
+      expect(service.userCredential).toEqual(credential);
+      expect(actualUser).toEqual({
+        uid: allUser[0].uid,
+        email: allUser[0].email,
+        displayName: allUser[0].fullName,
+        emailVerified: allUser[0].emailVerified,
+      });
+    } catch (error) {
+      expect(error).toBeFalsy();
+    }
+  });
+
+  it('User is login with incorrect password ', async () => {
+    let newUser: User = {
+      email: allUser[0].email,
+      password: 'invalidPassword',
+    };
+
+    try {
+      mockSignInFunctions(newUser);
+      const newUserCredential = await service.signIn(newUser);
+    } catch (error) {
+      expect(error.message).toEqual('The password is invalid');
+    }
+  });
+
+  it('User logged out', async () => {
+    mockSignOut(allUser[0].email);
+    const result = await service.signOut();
+    expect(service.isLogin).toBeFalsy();
+  });
 });

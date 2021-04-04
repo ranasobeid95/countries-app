6fbc24d680f4df9dda1335cbde9f75a2d65d3050
ranasobeid95 <ranasobeid95@gmail.com>
2021-04-04 19:41:46 +0300

    add canActivate for verify-email route #36

--- src/app/components/auth/auth-routing.module.ts
+++ src/app/components/auth/auth-routing.module.ts
@@ -4,12 +4,23 @@ import { RouterModule, Routes } from '@angular/router';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { VerifyEmailComponent } from './verify-email/verify-email.component';
+import { AuthGuard } from 'src/app/services/auth.guard';
 
 const routes: Routes = [
   { path: 'sign-out', redirectTo: 'sign-in', pathMatch: 'full' },
-  { path: 'sign-in', component: SignInComponent },
-  { path: 'sign-up', component: SignUpComponent },
-  { path: 'verify-email', component: VerifyEmailComponent },
+  {
+    path: 'sign-in',
+    component: SignInComponent,
+  },
+  {
+    path: 'sign-up',
+    component: SignUpComponent,
+  },
+  {
+    path: 'verify-email',
+    component: VerifyEmailComponent,
+    canActivate: [AuthGuard],
+  },
 ];
 
 @NgModule({

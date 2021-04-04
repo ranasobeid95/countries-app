8e08e14bba8f6b210c7189e767d272684abaa198
ranasobeid95 <ranasobeid95@gmail.com>
2021-04-04 19:49:18 +0300

    add signout method #36

--- src/app/components/shared/app-header/app-header.component.ts
+++ src/app/components/shared/app-header/app-header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ThemeService } from 'src/app/services/theme.service';
+import { AuthenticationService } from 'src/app/services/authentication.service';
 
 @Component({
   selector: 'app-header',
@@ -9,11 +10,15 @@ import { ThemeService } from 'src/app/services/theme.service';
 export class AppHeaderComponent implements OnInit {
   isDarkMode: boolean = false;
   isLogin: boolean = false;
-  constructor(private themeService: ThemeService) {}
+  constructor(
+    private themeService: ThemeService,
+    private auth: AuthenticationService
+  ) {}
 
   ngOnInit(): void {
     this.themeService.initTheme();
     this.isDarkMode = this.themeService.isDarkMode();
+    this.isLogin = this.auth.isLogin;
   }
 
   toggleDarkMode() {
@@ -21,5 +26,9 @@ export class AppHeaderComponent implements OnInit {
       ? this.themeService.update('light-mode')
       : this.themeService.update('dark-mode');
     this.isDarkMode = this.themeService.isDarkMode();
+  }
+
+  signOut() {
+    this.auth.signOut();
   }
 }

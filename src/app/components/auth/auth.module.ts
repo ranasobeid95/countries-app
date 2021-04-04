c429afa0279e2329061c39351747aea026aae46a
ranasobeid95 <ranasobeid95@gmail.com>
2021-04-04 19:47:00 +0300

    import SharedModule and add to imports array #36

--- src/app/components/auth/auth.module.ts
+++ src/app/components/auth/auth.module.ts
@@ -6,10 +6,11 @@ import { SignInComponent } from './sign-in/sign-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { VerifyEmailComponent } from './verify-email/verify-email.component';
 import { AuthRoutingModule } from '../auth/auth-routing.module';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [SignInComponent, SignUpComponent, VerifyEmailComponent],
-  imports: [CommonModule, MaterialModule, AuthRoutingModule],
+  imports: [CommonModule, MaterialModule, AuthRoutingModule, SharedModule],
   exports: [SignInComponent, SignUpComponent, VerifyEmailComponent],
 })
 export class AuthModule {}

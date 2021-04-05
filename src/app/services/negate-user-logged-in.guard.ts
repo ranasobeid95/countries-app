4b50f18eb81cb5224ed917346cf91c0b1871c458
ranasobeid95 <ranasobeid95@gmail.com>
2021-04-06 00:39:04 +0300

    generate guard to use it for protecting auth route after user logged in #29

---
+++ src/app/services/negate-user-logged-in.guard.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  UrlTree,
+  Router,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import {
+  MatSnackBarHorizontalPosition,
+  MatSnackBarVerticalPosition,
+  MatSnackBar,
+} from '@angular/material/snack-bar';
+import { IsAuthService } from './is-auth.service';
+import { ROUTES } from '../constants/routes';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class NegateUserLoggedInGuard implements CanActivate {
+  horizontalPosition: MatSnackBarHorizontalPosition = 'end';
+  verticalPosition: MatSnackBarVerticalPosition = 'top';
+  constructor(
+    private auth: IsAuthService,
+    private router: Router,
+    private _snackBar: MatSnackBar
+  ) {}
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    if (!this.auth.isAuth()) {
+      return true;
+    }
+    this._snackBar.open(`You already logged in !`, 'End now', {
+      duration: 5000,
+      horizontalPosition: this.horizontalPosition,
+      verticalPosition: this.verticalPosition,
+    });
+    this.router.navigate([`${ROUTES.COUNTRIES}`]);
+
+    return false;
+  }
+}
